015f1c82d800737a22fa2437fd5792cace997ae1
Delph <delph_teb@hotmail.com>
2020-11-28 00:48:54 -0500

    add save button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var currentDay = moment().format('dddd, MMMM Do YYYY');
+   var currentDay = moment().format('dddd, MMMM Do YYYY');
    $("#currentDay").text(currentDay); // Display current date in header at loading
    $("#currentTime").text(moment().format ('hh:mm a')); // Display current time in footer at loading
      
@@ -12,11 +12,11 @@ $(document).ready(function() {
             let hr = 9 + i;
             var row = $("<div></div>").attr({
                 "class": "row time-block",
-                "data-time": `${[hr]}`
+                "hour-block": `${[hr]}`
             });
             var col1 = $("<div></div>").attr({
-            "class": "hour px-0 col-sm-2",
-            "value": [hr]
+                "class": "hour px-0 col-sm-2",
+                "hour-block": `${[hr]}`
             });
 
             if (hr <= 12) {
@@ -25,9 +25,15 @@ $(document).ready(function() {
                col1.text(`${hr - 12}:00 p.m.`);
             };
 
-            var col2 = $("<textarea></textarea>").attr("class", "description col-sm-9");
+            var col2 = $("<textarea></textarea>").attr({
+                "class": "description col-sm-9",
+                "hour-block": `${[hr]}`
+            });
             
-            var col3 = $("<button></button>").attr("class", "saveBtn col-sm-1");
+            var col3 = $("<button></button>").attr({
+                "class": "saveBtn col-sm-1",
+                "hour-block": `${[hr]}` 
+            });
             col3.append(`<i class="fas fa-save"></i>`);
             
             row.append(col1, col2, col3); 
@@ -35,10 +41,10 @@ $(document).ready(function() {
         }     
     }
     
-
+    /*** Set the class of textarea ***/
     function statusColor() {
         $(".time-block").each(function() {
-            var hourBlock = parseInt($(this).attr("data-time"));
+            var hourBlock = parseInt($(this).attr("hour-block"));
             let textArea = $(".description", this);
             let currentHr = moment().hour()  // TOFIX: Static hr
             //Change the class based on the current time
@@ -50,10 +56,23 @@ $(document).ready(function() {
         });
     }
 
+    // Array for storage purposes
+    const EventList = JSON.parse(localStorage.getItem("eventsList")) || [];   // To store events for each hrs
 
 
-    createPlanning()
-    statusColor()
+    createPlanning();
+    statusColor();
+
+    $(".saveBtn").on("click", function(event) {
+        var blockIndex = parseInt($(event.target).closest(".time-block").attr("hour-block"));
+        var textArea = $(".description[hour-block='blockIndex']");  //TO FIX: Not working
+        
+        var eventTime = {
+            time: blockIndex,
+            event: textArea.value,
+        };
+        console.log(textArea.value);   // TO FIX : Testing
+     });
 
    /*** Update clock in footer ***/
    setInterval(function(){
