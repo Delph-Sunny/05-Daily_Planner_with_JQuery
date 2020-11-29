0dd552c3a413dfd69585a05e29039c050cfa595a
Delph <delph_teb@hotmail.com>
2020-11-28 23:43:06 -0500

    fix local storage, save button and display

--- README.md
+++ README.md
@@ -1,25 +1,23 @@
 # 05-Daily_Planner_with_JQuery
 ---
 
-:sweat_smile:
-
----
-
 ## Description 
 The webpage is a __simple daily planner__ created using JQeury. This app will allow a user to save events for each hour of the day.
 The following criteria are required for this planner:
 - [X] The current day is displayed at the top of the calendar.  
-- [ ] When scrolling down, the calendar contains time blocks for standard business hours.  
-- [ ] Each time block is color-coded to indicate whether it is in the past, present, or future.  
-- [ ] The user can enter an event into any time block. 
-- [ ] The event and its content is saved in local storage. 
-- [ ] The events are still displaying after refreshing the page. 
+- [X] When scrolling down, the calendar contains time blocks for standard business hours.  
+- [X] Each time block is color-coded to indicate whether it is in the past, present, or future.  
+- [X] The user can enter an event into any time block. 
+- [X] The event and its content is saved in local storage. 
+- [X] The events are still displaying after refreshing the page. 
 
 
 ## Installation
 
 Click on this [link] or download the entire repository, unzip the files and double-click on `index.html` to see the page in an internet browser.
-
+Enter a new event and click on the Save button to save the new event. Delete the event and click on the Save button to remove an event.
+The date can be found in the header of the page. 
+A clock displaying the current time was added in the footer.
 
 
 ## Mock-Up model
@@ -30,7 +28,7 @@ The following animation demonstrates the application functionalities needed for 
 
 ## Credits
 
-Trilogy Education Services for the image  
+Trilogy Education Services for the image.  
 Icons made by Freepik from [www.flaticon.com](https://www.flaticon.com/)
 
 
@@ -39,4 +37,4 @@ Icons made by Freepik from [www.flaticon.com](https://www.flaticon.com/)
 Copyright (c) 2020 DT. Everyone is permitted to use, copy, and modify verbatim copies of this document.
 
 ---
-[link]: https://delph-sunny.github.io/04-Daily_Planner_with_JQuery/
+[link]: https://delph-sunny.github.io/05-Daily_Planner_with_JQuery/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-   var currentDay = moment().format('dddd, MMMM Do YYYY');
-   $("#currentDay").text(currentDay); // Display current date in header at loading
-   $("#currentTime").text(moment().format ('hh:mm a')); // Display current time in footer at loading
-     
-   var container = $(".container").addClass("mb-5 pb-5");
+    var currentDay = moment().format('dddd, MMMM Do YYYY');
+    $("#currentDay").text(currentDay); // Display current date in header at loading
+    $("#currentTime").text(moment().format ('hh:mm a')); // Display current time in footer at loading
+    var eventList = {};                                  // To store all events
    
+    var container = $(".container").addClass("mb-5 pb-5");  
 
     /*** Build the planning ***/
     function createPlanning() {
@@ -18,7 +18,7 @@ $(document).ready(function() {
                 "class": "hour px-0 col-sm-2",
                 "hour-block": `${[hr]}`
             });
-
+            // Change the display for am/pm
             if (hr <= 12) {
                 col1.text(`${hr}:00 a.m.`);
             } else {
@@ -46,7 +46,7 @@ $(document).ready(function() {
         $(".time-block").each(function() {
             var hourBlock = parseInt($(this).attr("hour-block"));
             let textArea = $(".description", this);
-            let currentHr = moment().hour()  // TOFIX: Static hr
+            let currentHr = moment().hour()     // TOFIX: Static hr
             //Change the class based on the current time
             if (hourBlock < currentHr) {
                 textArea.addClass("past");
@@ -56,27 +56,44 @@ $(document).ready(function() {
         });
     }
 
-    // Array for storage purposes
-    const EventList = JSON.parse(localStorage.getItem("eventsList")) || [];   // To store events for each hrs
-
-
+    // Populate eventList object  
+    if (localStorage.getItem("eventsList")) {
+        eventList = JSON.parse(localStorage.getItem("eventsList"));
+    } else {        // if eventsList was empty, create eventList by populating its key time
+        eventList = {          
+            '9': { time: "9", eventText: ""},
+            '10':{ time: "10", eventText: ""},
+            '11':{ time: "11", eventText: ""},
+            '12':{ time: "12", eventText: ""},
+            '13':{ time: "13", eventText: ""},
+            '14':{ time: "14", eventText: ""},
+            '15':{ time: "15", eventText: ""},
+            '16':{ time: "16", eventText: ""},
+            '17':{ time: "17", eventText: ""}
+        };
+    }  
+    
     createPlanning();
     statusColor();
 
-    $(".saveBtn").on("click", function(event) {
-        var blockIndex = parseInt($(event.target).closest(".time-block").attr("hour-block"));
-        var textArea = $(`.description[hour-block="${blockIndex}"]`);  
-        
-        var eventTime = {
-            time: blockIndex,
-            event: textArea.text()   //TO FIX: Not working
-        };
-        console.log(textArea);   // TO FIX : Testing
+    /*** To display all previous events ***/
+    $(".time-block").each(function () {
+        let blockIndex = parseInt($(this).attr("hour-block"));
+        let text = eventList[blockIndex].eventText; 
+        $(`.description[hour-block="${blockIndex}"]`).val(text);
+    });
+
+    /*** To save after clicking on save button ***/
+    $(".saveBtn").on("click", function() {
+        let blockIndex = parseInt($(this).attr("hour-block"));
+        let textArea = $(`.description[hour-block="${blockIndex}"]`);
+        eventList[blockIndex].eventText= textArea.val();
+        localStorage.setItem("eventsList", JSON.stringify(eventList));
      });
 
-   /*** Update clock in footer ***/
-   setInterval(function(){
-        var currentTime = moment().format ('hh:mm a')
+    /*** Update clock in footer ***/
+    setInterval(function(){
+        let currentTime = moment().format ('hh:mm a')
         $("#currentTime").text(currentTime);    // Display current time in footer
     }, 1000);
 
